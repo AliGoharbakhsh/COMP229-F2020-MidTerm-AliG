cfd1e1308d799b10493b7e414272be47eb91d318
AliGoharbakhsh <agoharba@my.centennialcollege.ca>
2020-10-30 03:53:34 -0400

    Midterm first commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/book229"
+  "URI": "mongodb+srv://AliGohar:EyWLzsnm9Q76gRGv@mongodbserver1.kea7e.mongodb.net/book229?retryWrites=true&w=majority"
+};
